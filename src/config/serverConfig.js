4686f21bd6e72cb0072cf15cc9b012465ea4b642
Varun <vgupta168@gmail.com>
2023-07-13 00:50:51 +0530

    Setup of smtp using nodemailer done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    EMAIL_ID: process.env.EMAIL_ID,
+    EMAI_PASS: process.env.EMAI_PASS
 };
