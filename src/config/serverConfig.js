2a59458281d8affd79f6baded508b82fc025e9c9
Varun <vgupta168@gmail.com>
2023-07-13 04:41:24 +0530

    Added notification cron

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     EMAIL_ID: process.env.EMAIL_ID,
-    EMAI_PASS: process.env.EMAI_PASS
+    EMAIL_PASS: process.env.EMAIL_PASS
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 
 const {PORT} = require('./config/serverConfig');
 
-const setupAndStartServer = () => {
+const TicketController = require('./controllers/ticket-controller');
 
+const jobs = require('./utils/job');
+
+const setupAndStartServer = () => {
+    const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
+        jobs();
         if(process.env.DB_SYNC) {
             db.sequelize.sync({alter: true});
         }
